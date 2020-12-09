25effe6507d174496645b2d388c98524aaffc485
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-09 14:39:20 +0100

    sync

--- src/main/java/com/orsys/hibernate/main/HQLExample.java
+++ src/main/java/com/orsys/hibernate/main/HQLExample.java
@@ -1,10 +1,13 @@
 package com.orsys.hibernate.main;
 
+import java.util.List;
+
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import com.orsys.hibernate.model.Employee;
 import com.orsys.hibernate.util.HibernateUtil;
 
 public class HQLExample {
@@ -14,6 +17,9 @@ public static void main(String[] args)
 	Session session = sessionFactory.getCurrentSession();
 	Transaction tx = session.beginTransaction();
 	Query query = session.createQuery("from Employee");
-
+	List<Employee> empList = query.list();
+	for(Employee emp : empList){
+		System.out.println("List of Employees::"+emp.getId()+","+emp.getAddress().getCity());
+	}
 }
 }
