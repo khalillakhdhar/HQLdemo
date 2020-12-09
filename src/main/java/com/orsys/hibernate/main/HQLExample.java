d1e09ffda73ab9b28144a3e704b74cc903b17651
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-09 14:38:15 +0100

    list

--- src/main/java/com/orsys/hibernate/main/HQLExample.java
+++ src/main/java/com/orsys/hibernate/main/HQLExample.java
@@ -1,5 +1,19 @@
 package com.orsys.hibernate.main;
 
-public class HQLExample {
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import com.orsys.hibernate.util.HibernateUtil;
+
+public class HQLExample {
+public static void main(String[] args)
+{
+	SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+	Session session = sessionFactory.getCurrentSession();
+	Transaction tx = session.beginTransaction();
+	Query query = session.createQuery("from Employee");
+
+}
 }
